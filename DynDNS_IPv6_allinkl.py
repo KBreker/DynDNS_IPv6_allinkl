cd30e0c1c8c46c6ac5537900ffcf5b9ee947ccd0
Kai Breker <kai@kbreker.de>
2023-08-14 16:06:02 +0200

    Überarbeitung mit Konfigurationsdatei, nun Steuerung über Cronjob

--- DynDNS_IPv6_allinkl.py
+++ DynDNS_IPv6_allinkl.py
@@ -9,26 +9,21 @@
 #  herigen. Wenn sich diese geändert hat, wird der DynDNS-Service vom An-     #
 #  bieter allinkl.com über die neue IPv6-Adresse informiert.                  #
 #                                                                             #
-#  Kai Breker, Version 1.0.0 (14.08.2023)                                     #
+#  Kai Breker, Version 1.1.0 (14.08.2023)                                     #
 #                                                                             #
 ###############################################################################
 
-from subprocess import run
-import requests
-import time
+import configparser
+import os
 import sys
+from subprocess import run
+from datetime import datetime
 
-# Parameter anpassen
-interface = "eth0"
-dyndns_username = "insert_username_here"
-dyndns_password = "insert_password_here"
-dyndns_url = "dyndns.kasserver.com"
-sleep_timer = 5
-ipv6 = ""
+import requests
 
 
 def detect_ipv6():
-    global ipv6
+
     command_string = ["ip", "-6", "ad", "sh", f"{interface}"]
     ipv6_addresses = run(command_string, capture_output=True)
     if ipv6_addresses.returncode > 0:
@@ -45,7 +40,7 @@ def detect_ipv6():
 
 
 def send_new_ipv6(ipv6_new):
-    dyndns_string = f"https://{dyndns_username}:{dyndns_password}@{dyndns_url}/?myip6={ipv6}"
+    dyndns_string = f"https://{dyndns_username}:{dyndns_password}@{dyndns_url}/?myip6={ipv6_new}"
 
     result = requests.get(dyndns_string)
     if str(result) == "<Response [200]>":
@@ -55,15 +50,49 @@ def send_new_ipv6(ipv6_new):
     return
 
 
+def write_config(script_path, script_name):
+    with open(script_path + "/" + script_name + ".cfg", "w") as configfile:
+        config_p.set("IP", "IPv6", str(ipv6_actual))
+        config_p.set("IP", "timestamp", str(datetime.now()))
+        config_p.write(configfile)
+
+
 if __name__ == "__main__":
 
+    script_path = os.path.split(os.path.abspath(__file__))[0]
+    script_name = os.path.basename(__file__).split(".")[0]
+
+    try:
+        # Einstellungen für die Config-Datei
+        config_file = os.path.join(script_path, script_name + ".cfg")
+        config_p = configparser.ConfigParser()
+        config_p.read(config_file)
+
+        if not os.path.isfile(config_file):
+            raise FileNotFoundError
+
+        # Parameter anpassen
+        interface = config_p.get("General", "interface")
+        dyndns_username = config_p.get("Credentials", "dyndns_username")
+        dyndns_password = config_p.get("Credentials", "dyndns_password")
+        dyndns_url = config_p.get("Credentials", "dyndns_url")
+        ipv6_saved = config_p.get("IP", "ipv6")
+
+    except FileNotFoundError:
+        print(f"Die Konfigurationsdatei '{script_name}.cfg' konnte nicht "
+              "gefunden werden. Bitte überprüfe, ob Sie den gleichen Dateinamen "
+              "wie dieses Skript hat und im gleichen Verzeichnis liegt.")
+
+    except configparser.NoSectionError:
+        print(f"Die Konfigurationsdatei '{script_name}.cfg' konnte nicht "
+              "gelesen werden. Bitte überprüfe, ob die Struktur dem Beispiel "
+              "entspricht.")
+        sys.exit()
+
+    # IP-Adresse überprüfen
     ipv6_actual = detect_ipv6()
     print(f"Die aktuelle IPv6-Adresse lautet: {ipv6_actual}")
 
-    while True:
-        ipv6_new = detect_ipv6()
-
-        if ipv6_new != ipv6_actual:
-            send_new_ipv6(ipv6_new)
-            ipv6_actual = ipv6_new
-            time.sleep(sleep_timer)
+    if ipv6_saved != ipv6_actual:
+        send_new_ipv6(ipv6_actual)
+        write_config(script_path, script_name)
--- README.md
+++ README.md
@@ -1,21 +1,25 @@
 Dieses Programm ermittelt die vorherige IPv6-Adresse und vergleicht diese mit der vorherigen. Wenn sich diese geändert hat, wird der DynDNS / DDNS-Service vom Anbieter [all-inkl.com](https://all-inkl.com/) über die neue IPv6-Adresse informiert, damit auch eine Erreichbarkeit mittels IPv4 sichergestellt ist.
 
-Es wird das Modul 'requests' benötigt, das mittels 
+Es wird das Modul 'requests' und 'configparser' benötigt, die mittels 
 `python -m pip install requests`
+und
+`python -m pip install configparser`
 installiert werden kann.
 
-Im Code müssen folgende Einstellungen vorgenommen werden:
+In einer Konfigurationsdatei müssen noch Einstellungen vorgenommen werden. Die Datei *'DynDNS_IPv6_allinkl_example.cfg'* kann entsprechend angepasst werden. Die Konfigurationsdatei muss den gleichen Namen tragen, wie das Skript. Folgende Einstellungen sind erforderlich:
 
+**Abschnitt [General]**
 - **interface**: hier ist die Bezeichnung des abzufragenden Netzwerk-Adapters einzutragen, z.B: *eth0* oder *enp4s0*.
 
+**Abschnitt [Credentials]**
 Das Skript ist speziell für Kunden des Anbieters [all-inkl.com](https://all-inkl.com/) erstellt. Sofern der dortige Tarif des eigenen DDNS-Dienstes unterstützt, können unter *Tools*, *DDNS-Einstellungen* die erforderlichen Einstellungen entnommen werden.
 - **dyndns_username**: der Benutzername beginnt üblicherweise mit dyn...
 - **dyndns_password**: das Passwort kann ebenfalls dem KAS entnommen werden.
-- **dyndns_url**: der Server ist unter `dyndns.kasserver.com` zu erreichen und muss in der Regel nicht angepasst werden.
+- **dyndns_url**:  der Server ist unter `dyndns.kasserver.com` zu erreichen und muss in der Regel nicht angepasst werden.
 
-- **sleep_timer**: hier kann angegeben werden, in welchem Intervall (in Sekunden) die IPv6-Adresse auf eine Änderung überprüft werden soll.
+**Abschnitt [IP]**
+Diese Eintragungen bleiben leer - hier werden die ermittelte IP und der Zeitstempel eingetragen.
 
+Das Skript sollte dann über die Crontab ausgeführt werden, in diesem Beispiel jede Minute:
 
-Das Skript sollte dann über die Crontab bei einem Reboot ausgeführt werden:
-
-`@reboot python3 /pfad_zum_skript/DynDNS_IPv6_allinkl.py`
+`*/1 * * * *   python3 /pfad_zum_skript/DynDNS_IPv6_allinkl.py`
